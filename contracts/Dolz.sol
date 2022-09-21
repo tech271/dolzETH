3d268db852479352f65ebfc365097ef9d80aacca
David <david@Mac-mini-de-David.local>
2022-09-21 12:03:24 +0200

    New  contract, bridgeable and not mintable

--- contracts/Dolz.sol
+++ contracts/Dolz.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.16;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {IMintableERC20} from "./interface/IMintableERC20.sol";
 import {NativeMetaTransaction} from "./common/NativeMetaTransaction.sol";
 import {ContextMixin} from "./common/ContextMixin.sol";
 import {AccessControlMixin} from "./common/AccessControlMixin.sol";
@@ -12,37 +11,34 @@ contract Dolz is
     ERC20,
     AccessControlMixin,
     NativeMetaTransaction,
-    ContextMixin,
-    IMintableERC20
+    ContextMixin
 {
     bytes32 public constant PREDICATE_ROLE = keccak256("PREDICATE_ROLE");
 
-    constructor(
-        string memory name_,
-        string memory symbol_,
-        uint256 _initialSupply
-    ) ERC20(name_, symbol_) {
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply_)
+        ERC20(name_, symbol_)
+    {
         _setupContractId("Dolz");
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(PREDICATE_ROLE, _msgSender());
 
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _setupRole(PREDICATE_ROLE, _msgSender());
-
-        _mint(_msgSender(), _initialSupply);
+        _mint(_msgSender(), initialSupply_);
         _initializeEIP712(name_);
     }
 
-    /**
-     * @dev See {IMintableERC20-mint}.
-     */
-    function mint(address user, uint256 amount)
-        external
-        override
-        only(PREDICATE_ROLE)
-    {
-        _mint(user, amount);
+    /// @dev Allow user to burn his token
+    /// @param _amount Amount of tokens to burn
+    function burn(uint256 _amount)
+    external {
+        _burn(_msgSender(), _amount);
     }
 
-    function _msgSender() internal view virtual override returns (address) {
+    function _msgSender()
+    internal
+    view
+    virtual
+    override
+    returns (address) {
         return ContextMixin.msgSender();
     }
 }
